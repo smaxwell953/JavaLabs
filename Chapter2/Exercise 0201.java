e87f2c55f0ac902af44336ecb99ef9e70f1dd659
Sara Maxwell <saraann.maxwell@gmail.com>
2020-05-05 23:51:46 -0600

    Added Chapter 2 exercise 1

---
+++ Chapter2/Exercise 0201.java
@@ -0,0 +1,15 @@
+/* Exercise 2.1. Write code that converts Fahrenheit to Celsius. The formula for conversion is: C = (5/9) * (F - 32). */ 
+
+import java.util.Scanner;
+public class ConvertCeltoFah
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the temperature in Celsius: ");
+		double fahrenheit;
+		fahrenheit = input.nextDouble();
+		double celsius = ( (5 / 9) * (fahrenheit - 32);
+		System.out.println("The temperature in Fahrenheit is " + fahrenheit);	
+	}
+}
