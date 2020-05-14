416ea270cf0028277a2ec52d66026db30d4528af
Sara Maxwell <saraann.maxwell@gmail.com>
2020-05-13 22:59:47 -0600

    Displays "Welcome to Java" in block letters

---
+++ Chapter2/Exercise 0203.java
@@ -0,0 +1,12 @@
+/*Write a program that displays "Welcome to Java!" in large block letters, each letter made up of the same character it represents. The letters should be six printed lines each.*/
+
+public class Exercise0203 {
+	public static void main (String[] args) {         
+		System.out.println (null,"   W               W  EEEEEE  L           CCCC        OOOO      M     M  EEEEEE       TTTTTTT     OOOO            JJJJJ      A     V         V     A      !!",
+		System.out.println (null,"    W             W   E       L         CC    CC    OO    OO    MM   MM  E               T      OO    OO            J       A A     V       V     A A     !!",
+		System.out.println (null,"     W     W     W    EEEE    L        C           O        O   M M M M  EEEE            T     O        O           J      A   A     V     V     A   A    !!",
+		System.out.println (null,"      W   W W   W     E       L        C           O        O   M  M  M  E               T     O        O      J    J     A     A     V   V     A     A   !!",
+		System.out.println (null,"       W W   W W      E       L         CC    CC    OO    OO    M     M  E               T      OO    OO        J   J    AAAAAAAAA     V V     AAAAAAAAA    ",          
+		System.out.println (null,"        W     W       EEEEEE  LLLLLLL     CCCC        OOOO      M     M  EEEEEE          T        OOOO           JJJ    A         A     V     A         A !!",
+	}
+}
